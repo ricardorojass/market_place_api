5cf70686e2876e2753e1ddcf4064099734ec3aa6
Ricardo Rojas <reddrum7@gmail.com>
2015-09-22 16:10:19 -0500

    finished setting-api chapter 2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '4.2.3'
-gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -18,6 +17,12 @@ end
 
 group :development do
   gem 'sqlite3'
+end
+
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
 end
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
-Rails.application.routes.draw do
-  #Api definition
-  namespace :api do
+require 'api_constraints'
 
+MarketPlaceApi::Application.routes.draw do
+  #Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
   end
 end
